f7a768b96fc5a1d6264ef19a676a1eec9414558f
akash patel <akash.patel@hurix.com>
2022-05-12 22:02:59 +0530

    list and Keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import HookForm from './react-hook-form/HookForm';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import HookForm from './react-hook-form/HookForm';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import ListKeys from './ListAndKeys/Listkeys'
 function App() {
-  const notify = () => toast("Wow so easy!");
- return (
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+return (
     <div>
-      <HookForm/>
+      {/* <HookForm/> */}
+      {/* <ListKeys arr = {arr}/> */}
+      <ListKeys/>
       {/* <button onClick={notify}>Notify!</button>
         <ToastContainer /> */}
     </div>
