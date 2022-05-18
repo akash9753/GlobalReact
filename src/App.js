79421c8df054210b8ec1bebab64a5f7485814867
akash patel <akash.patel@hurix.com>
2022-05-18 10:27:25 +0530

    CompositionvsInheritance

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import LiftingStateUp from './LiftingStateUp1/LiftingStateUp';
 import ListKeys from './ListAndKeys/Listkeys'
 import Test from './LiftingStateUp1/Test';
 import Employee from './LiftingStateUp2/Employee';
+import CvsI from './CompositionVsInheritance/CvsI';
+import Ex1 from './CompositionVsInheritance/Ex1';
+import Ex2 from './CompositionVsInheritance/Ex2';
+import Ex3 from './CompositionVsInheritance/Ex3';
 function App() {
   // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   const state = {
@@ -19,9 +23,20 @@ return (
       {/* <ListKeys/> */}
       {/* <button onClick={notify}>Notify!</button>
         <ToastContainer /> */}
-      <LiftingStateUp name={state.name}/>
+      {/* <LiftingStateUp name={state.name}/>
       <Test name={state.name}/>
-      <Employee/>
+      <Employee/> */}
+      <CvsI/>
+      <hr/>
+      <h2>sending data in function as props</h2>
+      <Ex1/>
+      <hr/>
+      <h2>sending big data in function as props</h2>
+      <Ex2/>
+      <hr/>
+      <h2>sending component as props to another component which is inside a class or function</h2>
+      <Ex3/>
+      <hr/>
     </div>
   )
 }
